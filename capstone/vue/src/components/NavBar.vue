6aebb9c768935c379e080308dd201de985630f4a
Jeff Winograd <jwinograd1@gmail.com>
2023-01-31 15:48:28 -0600

    navbar mocked-up

--- capstone/vue/src/components/NavBar.vue
+++ capstone/vue/src/components/NavBar.vue
@@ -6,58 +6,41 @@
     >
     <v-row align-items="center">
     <v-icon large>mdi-spa</v-icon>
-    <v-toolbar-title class="ml-5"> <h2>ClimbIt</h2> </v-toolbar-title>
+    <v-toolbar-title class="ml-5"> <h2 @click="goToHome">ClimbIt</h2> </v-toolbar-title>
 
     <v-spacer></v-spacer>
 
     <v-btn icon> <v-icon>mdi-magnify</v-icon></v-btn>
 
-  
-    
-        <!-- <v-text-field 
-        solo
-        label="search routes"
-        append-inner-icon="mdi-search"
-        ></v-text-field> -->
 
-        <v-app-bar-nav-icon large>
-          
+      <v-menu transition="scroll-y-transition">
+        <template v-slot:activator="{ on, attrs }">
+        <v-app-bar-nav-icon large
+            v-bind="attrs"
+            v-on="on">
+
         </v-app-bar-nav-icon>
-    
+        </template>
+         <v-list color="green">
+          <v-list-item exact @click="goToProfile"
+            >
+            <v-list-item-title >View Profile</v-list-item-title>
+          </v-list-item>
+          <v-divider></v-divider>
+            <v-list-item
+            link>
+            <v-list-item-title>Search Routes</v-list-item-title>
+          </v-list-item>
+          <v-divider></v-divider>
+            <v-list-item exact @click="goToLogout">
+            
+            <v-list-item-title >LogOut</v-list-item-title>
+          </v-list-item>
+
+        </v-list>
+      </v-menu> 
 
 
-    
-
-      <!-- <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn> -->
       </v-row>
     </v-app-bar>
 </template>
@@ -65,6 +48,18 @@
 <script>
 export default {
     name: "navbar",
+    methods: {
+      goToProfile(){
+        this.$router.push({name: 'jeffplay'});
+      },
+        goToLogout(){
+        this.$router.push({name: 'logout'});
+      },
+        goToHome(){
+        this.$router.push({name: 'home'});
+      },
+
+    }
 
 }
 </script>
