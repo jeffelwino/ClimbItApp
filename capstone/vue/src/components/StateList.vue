f4a4c56735129c28f69c06af7916acf416913c75
Jeff Winograd <jwinograd1@gmail.com>
2023-01-31 12:28:24 -0600

    added sandboxes in router

---
+++ capstone/vue/src/components/StateList.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+
+      <v-simple-table>
+          <tbody>
+        <tr
+          v-for="state in $store.state.states"
+          v-bind:key="state.name"
+        >
+          <td>{{ state.name }}</td>
+          <td>{{ state.name }}</td>
+          <td>{{ state.name }}</td>
+          <td>{{ state.name }}</td>
+          <td>{{ state.name }}</td>
+          
+        </tr>
+      </tbody>
+      </v-simple-table>
+
+
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: "state-list",
+
+}
+</script>
+
+<style>
+
+</style>
