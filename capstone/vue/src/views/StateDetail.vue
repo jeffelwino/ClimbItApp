0aad1503ca07ba5dd327263fb8ee1ac5fdea5a98
Jeff Winograd <jwinograd1@gmail.com>
2023-01-31 16:32:25 -0600

    Added StateInfo Component

--- capstone/vue/src/views/StateDetail.vue
+++ capstone/vue/src/views/StateDetail.vue
@@ -1,39 +1,19 @@
 <template>
   <div>
-      <h2>Welcome To {{state.name}}</h2>
-      <p>We have {{state.areas}} climbing areas</p>
+      
+      <state-info />
+     
   </div>
 </template>
 
 <script>
+import StateInfo from "../components/StateInfo";
 export default {
     name: 'state-detail',
-    data() {
-        return{
-            state: {
-                abbrev: '',
-                name: '',
-                areas: 0
-            }
-
-        };
-    },
-    methods:
-    {
-        loadState() {
-            this.state = this.$store.state.states.find((estado) => {
-                return estado.abbrev == this.$route.params.abbrev;
-            });
-        },
-    },
-created() {
-    this.loadState();
-}
+    components: {StateInfo},
+   
 };
     
-
-
-
 </script>
 
 <style>
