b7e2bd5b066d4359b800759342eb91c0b4adc7ce
Michael Eden <edentastic@gmail.com>
2023-01-31 14:43:07 -0500

    added data to the store for routes, walls, crags

--- capstone/vue/src/store/index.js
+++ capstone/vue/src/store/index.js
@@ -12,7 +12,7 @@ Vue.use(Vuex)
 const currentToken = localStorage.getItem('token')
 const currentUser = JSON.parse(localStorage.getItem('user'));
 
-if(currentToken != null) {
+if (currentToken != null) {
   axios.defaults.headers.common['Authorization'] = `Bearer ${currentToken}`;
 }
 
@@ -21,47 +21,57 @@ export default new Vuex.Store({
     token: currentToken || '',
     user: currentUser || {},
     states: [
-      { abbrev: "AL",
+      {
+        abbrev: "AL",
         name: "Alabama",
-        areas: 100  
+        areas: 100
       },
-      { abbrev: "AK",
+      {
+        abbrev: "AK",
         name: "Alaska",
         areas: 151
       },
-      { abbrev: "AZ",
-      name: "Arizona",
-        areas: 200 
+      {
+        abbrev: "AZ",
+        name: "Arizona",
+        areas: 200
       },
-      { abbrev: "AK",
-      name: "Arkansas",
-        areas: 300  
+      {
+        abbrev: "AK",
+        name: "Arkansas",
+        areas: 300
       },
-       { abbrev: "CA",
-       name: "California",
-        areas: 400  
+      {
+        abbrev: "CA",
+        name: "California",
+        areas: 400
       },
-        { abbrev: "CO",
+      {
+        abbrev: "CO",
         name: "Colorado",
-        areas: 100  
+        areas: 100
       },
-      { abbrev: "CN",
+      {
+        abbrev: "CN",
         name: "Connecticut",
         areas: 151
       },
-      { abbrev: "DE",
+      {
+        abbrev: "DE",
         name: "Delaware",
-        areas: 200 
+        areas: 200
       },
-      { abbrev: "FL",
+      {
+        abbrev: "FL",
         name: "Florida",
-        areas: 300  
+        areas: 300
       },
-       { abbrev: "GA",
+      {
+        abbrev: "GA",
         name: "Georgia",
-        areas: 400  
+        areas: 400
       },
-  ],
+    ],
     areas: [
       {
         name: 'a1',
@@ -96,10 +106,40 @@ export default new Vuex.Store({
         area_name: 5,
         description: 'The famous seals singing their opera',
         latitude: 41.506348,
-        longitude:  -81.609336
+        longitude: -81.609336
       }
-    ]
-    
+    ],
+    crags: [
+      { id: 1, area_id: 1, name: 'Mad River Gorge', description: 'The finest climbing Ohio has to offer', latitude: 39.912833, longitude: -83.860439 },
+      { id: 2, area_id: 1, name: 'Muir Valley', description: 'A luxury climbign experience', latitude: 37.724503, longitude: -83.634545 },
+      { id: 3, area_id: 1, name: 'Upper Meadow', description: 'Beautiful area upstream on the Meadow River', latitude: 38.151515, longitude: -80.926571 },
+      { id: 4, area_id: 1, name: 'Marne Road', description: 'Roadside climbing in Newark, Ohio', latitude: 40.073522, longitude: -82.259237 },
+    ],
+    walls: [
+      { id: 1, crag_id: 1, name: 'Amusement Wall', description: 'Fun climbing, seeps for awhile after it rains. Busy on nice days' },
+      { id: 2, crag_id: 1, name: "Dad's Wall", description: "Get here early if you're looking to warm up" },
+      { id: 3, crag_id: 1, name: 'Trash Shoot Area', description: 'Good variety, some spicy climbs for their grade' },
+      { id: 4, crag_id: 2, name: 'Bruise Brothers', description: 'An iconic starting place for new climbers, avoid on busy days' },
+      { id: 5, crag_id: 3, name: 'Zero Buttress', description: 'Watch the runouts, but some very nice vertical climbing' },
+      { id: 6, crag_id: 4, name: 'Katz Wall', description: 'Good moderate climbing, and some taller walls especially for Ohio' },
+
+    ],
+    routes: [
+      { id: 1, wall_id: 1, name: 'Slappy McCracken', grade: '5.10b', height: '30ft', style: 'sport' },
+      { id: 2, wall_id: 1, name: 'Knope We Can', grade: '5.7', height: '25ft', style: 'sport' },
+      { id: 3, wall_id: 2, name: 'Chain Mail Hula Skirt', grade: '5.7', height: '25ft', style: 'trad' },
+      { id: 4, wall_id: 2, name: 'Excellent Ella', grade: '5.6', height: '25ft', style: 'sport' },
+      { id: 5, wall_id: 3, name: 'Digitalis', grade: '5.11d', height: '25ft', style: 'sport' },
+      { id: 6, wall_id: 3, name: 'Chin Chimney', grade: '5.8', height: '25ft', style: 'sport' },
+      { id: 7, wall_id: 4, name: 'Send Me On My Way', grade: '5.9', height: '75ft', style: 'sport' },
+      { id: 8, wall_id: 4, name: 'Rat Stew', grade: '5.10a', height: '75ft', style: 'sport' },
+      { id: 9, wall_id: 5, name: 'Nasty Weiners', grade: '5.10a', height: '65ft', style: 'sport' },
+      { id: 10, wall_id: 5, name: 'Infatuation', grade: '5.7', height: '60ft', style: 'sport' },
+      { id: 11, wall_id: 6, name: 'Katz Corner', grade: '5.6', height: '35ft', style: 'sport' },
+      { id: 12, wall_id: 6, name: 'Silver Streaks', grade: '5.9', height: '60ft', style: 'sport' },
+
+    ],
+
   },
   mutations: {
     SET_AUTH_TOKEN(state, token) {
@@ -109,7 +149,7 @@ export default new Vuex.Store({
     },
     SET_USER(state, user) {
       state.user = user;
-      localStorage.setItem('user',JSON.stringify(user));
+      localStorage.setItem('user', JSON.stringify(user));
     },
     LOGOUT(state) {
       localStorage.removeItem('token');
