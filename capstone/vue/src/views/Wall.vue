3e8f21eb06cc32b2a708baa61d187103c06e5d64
Michael Eden <edentastic@gmail.com>
2023-01-31 17:34:26 -0500

    Wall page with wall details and links to routes done

---
+++ capstone/vue/src/views/Wall.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wall">
+    <h2>Wall Page</h2>
+    <wall-detail v-bind:wall="wall" />
+  </div>
+</template>
+
+<script>
+import WallDetail from "../components/WallDetail.vue";
+export default {
+  name: "wall",
+  components: { WallDetail },
+  data() {
+    return {
+      wall: {
+        id: "",
+        crag_id: "",
+        name: "",
+        description: "",
+      },
+    };
+  },
+  methods: {
+    loadWall() {
+      this.wall = this.$store.state.walls.find((w) => {
+        return w.id == this.$route.params.id;
+      });
+    },
+  },
+  created() {
+    this.loadWall();
+  },
+};
+</script>
+
+<style>
+</style>
