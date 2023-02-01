1db461392b4af3a9fcf05e96a825ff70855309d3
Allan Treat <allantreat73@gmail.com>
2023-02-01 15:13:57 -0500

    just in case 3

--- capstone/vue/src/components/routecomps/TickBox.vue
+++ capstone/vue/src/components/routecomps/TickBox.vue
@@ -1,92 +1,63 @@
 <template>
-    <v-sheet>
-    <v-dialog
-      v-model="dialog"
-      persistent
-      max-width="600px"
-    >
-    <template v-slot:activator="{ on, attrs }">
-    <v-btn class= "light-green" small 
-    v-bind="attrs"
-          v-on="on">
-      ClimbedIt!
-    </v-btn>
-    </template>
-<v-card>
-  <v-form class="px-3">
-    <v-card-title class="dark-grey">
-        <span class="text-h4">Congrats! Tell us more!</span>
-    </v-card-title>
-    <v-divider></v-divider>
-    <v-row class=" justify-center">
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-         <v-card-text outlined v-bind:key="survey.routeName">{{survey.routeName}} </v-card-text>
-         </v-col>
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-        <label for="date-climbed" id="date-climbed">Date Climbed:</label>
-        <input class="mt-5 ml-2" name="date-climbed" id="date-climbed" type="date" v:model="survey.dateClimbed"/> 
-        </v-col>
-    </v-row>
-    <v-row class="inline-block">
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-        <v-card-text>
-            Rating:
-        <v-rating
-          v-model="survey.rating"
-          color="yellow darken-3"
-          background-color="grey darken-1"
-          empty-icon="$ratingFull"
-          hover
-          medium
-        ></v-rating>
-        </v-card-text>
-        </v-col>
-          <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-        <v-textarea outlined label="notes" v:model="survey.notes"></v-textarea>
-        </v-col>
-        </v-row>
-        <v-row class="justify-space-around">
-            <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Cancel
-          </v-btn>
-          
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Submit
-          </v-btn>
+  <v-sheet>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn class="light-green" small v-bind="attrs" v-on="on">
+          ClimbedIt!
+        </v-btn>
+      </template>
+      <v-card>
+        <v-form class="px-3">
+          <v-card-title class="dark-grey">
+            <span class="text-h4">Congrats! Tell us more!</span>
+          </v-card-title>
+          <v-divider></v-divider>
+          <v-row class="justify-center">
+            <v-col cols="12" sm="6" md="3">
+              <v-card-text outlined v-bind:key="survey.routeName"
+                >{{ survey.routeName }}
+              </v-card-text>
+            </v-col>
+            <v-col cols="12" sm="6" md="3">
+              <label for="date-climbed" id="date-climbed">Date Climbed:</label>
+              <input
+                class="mt-5 ml-2"
+                name="date-climbed"
+                id="date-climbed"
+                type="date"
+                v:model="survey.dateClimbed"
+              />
+            </v-col>
+          </v-row>
+          <v-row class="inline-block">
+            <v-col cols="12" sm="6" md="3">
+              <v-card-text>
+                Rating:
+                <v-rating
+                  v-model="survey.rating"
+                  color="yellow darken-3"
+                  background-color="grey darken-1"
+                  empty-icon="$ratingFull"
+                  hover
+                  medium
+                ></v-rating>
+              </v-card-text>
+            </v-col>
+            <v-col cols="12" sm="6" md="3">
+              <v-textarea
+                outlined
+                label="notes"
+                v:model="survey.notes"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+          <v-row class="justify-space-around">
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Cancel
+            </v-btn>
 
-        </v-row>
-         
-</v-form>
-</v-card>
-     </v-dialog>
-    </v-sheet>
-
-            <v-btn color="blue darken-1" text @click="addSurvey">
-              Submit <!--We want to grab the current route_id  -->
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Submit
             </v-btn>
           </v-row>
         </v-form>
@@ -97,7 +68,7 @@
 
 <script>
 export default {
-    name: 'tickbox',
+  name: "tickbox",
   data() {
     return {
       dialog: false,
@@ -113,16 +84,15 @@ export default {
 
   methods: {
     addSurvey() {
-      this.dialog=false;
+      this.dialog = false;
       this.survey = {
         routeName: "",
         dateClimbed: "",
         rating: 0,
         notes: "",
       };
-      
-    }
-  }
+    },
+  },
 };
 </script>
 
