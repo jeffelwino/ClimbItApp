483b4666df30bc2d2e088605926de9d7fe539597
Jeff Winograd <jwinograd1@gmail.com>
2023-02-01 09:24:59 -0600

    Working on RouteHeader

---
+++ capstone/vue/src/components/routecomps/RouteHeader.vue
@@ -0,0 +1,44 @@
+<template>
+ <div class="route-header">
+     <!-- route name and grade box -->
+     <v-container class="blue">
+
+     <v-row class="justify-center blue">
+         <h3 class="white--text">
+        {{ route.name }} ({{ route.grade }})
+        </h3>
+    
+     </v-row>
+     <v-row class="mt-10 justify-center">
+    <v-btn>
+      ClimbedIt!
+    </v-btn>
+     </v-row>
+
+      
+     <v-card class="blue">
+            
+
+     </v-card>
+       
+    
+
+     </v-container>
+    
+     <!-- tick box button -->
+     <!-- add to to-do list button -->
+
+ </div>
+</template>
+
+<script>
+export default {
+    name: 'route-header',
+    props: ["route"],
+
+}
+</script>
+
+<style>
+
+</style>
--- capstone/vue/src/views/RoutePage.vue
+++ capstone/vue/src/views/RoutePage.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="route">
-    <h2>Route Page</h2>
-    <h2>{{ route.name }} ({{ route.grade }})</h2>
+  
+    
+    <route-header v-bind:route="route" />
     <route-detail v-bind:route="route" />
   </div>
 </template>
 
 <script>
-import RouteDetail from "../components/RouteDetail.vue";
+import RouteDetail from "../components/routecomps/RouteDetail.vue";
+import RouteHeader from "../components/routecomps/RouteHeader.vue";
 export default {
   name: "route",
-  components: { RouteDetail },
+  components: { RouteDetail, RouteHeader },
   data() {
     return {
       route: {
