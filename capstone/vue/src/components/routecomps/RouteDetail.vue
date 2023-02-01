c5b906ad533bd29663392c28bbd788cfb0fbd1e3
Jeff Winograd <jwinograd1@gmail.com>
2023-02-01 17:50:35 -0600

    added average review to RouteDetail

--- capstone/vue/src/components/routecomps/RouteDetail.vue
+++ capstone/vue/src/components/routecomps/RouteDetail.vue
@@ -1,38 +1,63 @@
 <template>
   
-    <div class="stats ma-5" style="width: 90%">
-      <v-card class="grey justify-center">
-      <h3>Stats:</h3> 
-      <v-row>
-        <v-col
+    <v-container>
+      <v-card class="ma-5 justify-center" border="2px solid black" width="90%">
+      <v-card-title> <h3>Stats:</h3>  </v-card-title>
+      <v-card-text>
+      <v-layout>
+        <!-- <v-col
           cols="12"
           sm="6"
-          md="3">
-          <ul>
+          md="3"> -->
+        <ul>
         <li>Height: {{ route.height }}</li>
         <li>Style: {{ route.style }}</li>
-      </ul>
-          </v-col>
-          <v-col
-          cols="12"
+        </ul>
+      
+          <!-- </v-col>
+          <v-col -->
+          <!-- cols="12"
           sm="6"
-          md="3">
-          <ul>
-        <li>Rating (stars)</li>
+          md="3"> -->
+          <ul  class="ml-10">
+        <li>Rating: 
+          
+           <v-rating
+             readonly
+            v-model="averageRating"
+            color="yellow darken-3"
+            background-color="grey darken-1"
+            empty-icon="$ratingFull"
+            hover
+            x-small>
+        </v-rating>
+        </li>
         <li>Protection</li>
       </ul>
-          </v-col>
-      </v-row>
+          <!-- </v-col> -->
+      </v-layout>
+      </v-card-text>
       </v-card>
-    </div>
+    </v-container>
  
 </template>
 
 <script>
+
 export default {
   name: "route-detail",
   props: ["route"],
-  components: {},
+
+  computed: {
+        averageRating() {
+            const ticks = this.$store.state.ticks.filter((t) => {
+            return t.route_id == this.route.id;});
+            let sum = ticks.reduce((currentSum, tick) => {
+            return currentSum + tick.rating;
+            }, 0);
+            return(sum / ticks.length).toFixed(2);
+        }
+        },
   data() {
     return {};
   },
