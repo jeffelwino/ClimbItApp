f0538d35e47aa02a152302df3ccdd7a8cebe57d7
Michael Eden <edentastic@gmail.com>
2023-02-03 09:11:01 -0500

    made route cards links

--- capstone/vue/src/components/wall/RouteCard.vue
+++ capstone/vue/src/components/wall/RouteCard.vue
@@ -1,38 +1,42 @@
 <template>
-  <v-card>{{route.name}} ({{route.grade}})           
-      <v-rating
-           readonly
-           v-model="averageRating"
-           half-increments
-           color="yellow darken-3"
-           background-color="grey darken-1"
-           empty-icon="$ratingFull"
-           ></v-rating>
-    </v-card>
+  <v-card @click="navigateToRoute()"
+    >{{ route.name }} ({{ route.grade }})
+    <v-rating
+      readonly
+      v-model="averageRating"
+      half-increments
+      color="yellow darken-3"
+      background-color="grey darken-1"
+      empty-icon="$ratingFull"
+    ></v-rating>
+  </v-card>
 </template>
 
 <script>
-
 export default {
-    name: "route-card",
-    props: ["route"], 
-    computed: {
-        ticks(){
-            return this.$store.state.ticks.filter(tick => {
-                return tick.routeId == this.route.id;
-            }
-        )},
-        averageRating(){
-            let sum = 0
-            this.ticks.forEach(tick => {
-                sum += tick.rating
-            })
-            return sum /= this.ticks.length
-        }
-    }
-}
+  name: "route-card",
+  props: ["route"],
+  computed: {
+    ticks() {
+      return this.$store.state.ticks.filter((tick) => {
+        return tick.routeId == this.route.id;
+      });
+    },
+    averageRating() {
+      let sum = 0;
+      this.ticks.forEach((tick) => {
+        sum += tick.rating;
+      });
+      return (sum /= this.ticks.length);
+    },
+  },
+  methods: {
+    navigateToRoute() {
+      this.$router.push({ name: "route-page", params: { id: this.route.id } });
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
