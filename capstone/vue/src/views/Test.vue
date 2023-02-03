c1abbf4b5632fbb26242f571446d19ae826b7441
Michael Eden <edentastic@gmail.com>
2023-02-03 13:02:04 -0500

    edits in store

--- capstone/vue/src/views/Test.vue
+++ capstone/vue/src/views/Test.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="test">
     <h1>THIS IS MICHAEL'S OWN PERSONAL PRIVATE VIEW</h1>
+    <h2 v-if="this.$store.state.user.authorities[0].name == 'ROLE_ADMIN'">
+      THIS IS VISIBLE TO ADMINS
+    </h2>
   </div>
 </template>
 
@@ -26,7 +29,8 @@ export default {
     };
   },
   created() {
-    console.log(this.$store.state.user.id);
+    console.log(this.$store.state.user);
+    console.log(this.$store.state.user.authorities[0].name);
   },
 };
 </script>
