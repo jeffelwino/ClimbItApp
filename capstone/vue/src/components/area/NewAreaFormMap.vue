b1949922d33b06df690556e7069625943e97b8bf
Allan Treat <allantreat73@gmail.com>
2023-02-06 11:11:04 -0500

    trying to fix the clicking ability for adding a new marker

--- capstone/vue/src/components/area/NewAreaFormMap.vue
+++ capstone/vue/src/components/area/NewAreaFormMap.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="new-area-form-map">
-    <h2>Search and add a pin</h2>
-    <!-- <GmapAutocomplete @place_changed="setPlace" /> -->
-    <button @click.prevent="addMarker">Add Area</button>
+    <div id="map-header">
+      <GmapAutocomplete @place_changed="setPlace" id="search-bar" />
+      <div id="buttons" class="coord-input">
+        <input v-on:click="addPinViaInput" type="button" value="Add Marker" />
+      </div>
+    </div>
     <GmapMap
       :center="mapCenter"
       :zoom="7"
       style="width: 100%; height: 400px"
-      id="area-map"
+      id="map"
     >
-      <GmapMarker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        @click="center = m.position"
-    /></GmapMap>
+    </GmapMap>
   </div>
 </template>
 
@@ -32,12 +30,16 @@ export default {
     };
   },
   mounted() {
-    this.geolocate();
+    // this.geolocate();
+    this.initMap();
+    this.dropPins();
   },
   methods: {
-   initMap() {
-
-      this.map = new window.google.maps.Map(document.getElementId("map"), {
+    setPlace(place) {
+      this.currentPlace = place;
+    },
+    initMap() {
+      this.map = new window.google.maps.Map(document.getElementById("map"), {
         center: this.mapCenter,
         zoom: 7,
         maxZoom: 20,
@@ -48,19 +50,21 @@ export default {
         fullscreenControl: true,
       });
 
-        let noPOIStyle = [{
+      let noPOIStyle = [
+        {
           featureType: "poi",
           elementType: "labels",
-          stylers: [{ visibility: "off"}],
-        }];
-        this.map.setOptions({ styles: noPOIStyle});
-        window.google.maps.event.addListener(this.map, "rightclick", (event) => {this.addPinViaClick(event);
+          stylers: [{ visibility: "off" }],
+        },
+      ];
+      this.map.setOptions({ styles: noPOIStyle });
+      window.google.maps.event.addListener(this.map, "rightclick", (event) => {
+        this.addPinViaClick(event);
       });
-
     },
 
     makeMarkerObj(latLng, name) {
-      const markerObj = {coord: latLng, name: name};
+      const markerObj = { coord: latLng, name: name };
       return markerObj;
     },
 
@@ -70,8 +74,51 @@ export default {
       this.places.push(markerObj);
       this.dropPin(markerObj);
     },
-    
-    
+    addPinViaInput() {
+      let latitudeValue = parseFloat(
+        document.getElementById("latitude-input").value
+      );
+      let longitudeValue = parseFloat(
+        document.getElementById("longitude-input").value
+      );
+      const latSelectIndex =
+        document.getElementById("latitude-direction").selectedIndex;
+      const lonSelectIndex = document.getElementById(
+        "longitude-direction"
+      ).selectedIndex;
+      const latitudeDirection =
+        document.getElementById("latitude-direction").options[latSelectIndex]
+          .text;
+      const longitudeDirection = document.getElementById("longitude-direction")
+        .options[lonSelectIndex].text;
+      const description = document.getElementById("description-input").value;
+      latitudeValue =
+        latitudeDirection === "North" ? latitudeValue : -1 * latitudeValue;
+      longitudeValue =
+        longitudeDirection === "East" ? longitudeValue : -1 * longitudeValue;
+      const markerObj = this.makeMarkerObj(
+        { lat: latitudeValue, lng: longitudeValue },
+        description
+      );
+      this.places.push(markerObj);
+      this.dropPin(markerObj);
+    },
+
+    dropPins() {
+      this.places.forEach((x) => this.dropPin(x));
+    },
+
+    dropPin(markerObj) {
+      new window.google.maps.Marker({
+        position: markerObj.coord,
+        map: this.map,
+        label: {
+          text: markerObj.name,
+          color: "blue",
+        },
+      });
+    },
+
     geolocate: function () {
       navigator.geolocation.getCurrentPosition((position) => {
         this.center = {
@@ -80,13 +127,18 @@ export default {
         };
       });
     },
-    addArea() {
-      //This will push the new created crags to the crag store
-      // this.$router.area.push(this.places) <-- or something like this...
-    },
   },
 };
 </script>
 
 <style>
+#map-header {
+  display: flex;
+  grid-template-columns: 1fr 1fr;
+  
+}
+#buttons, #search-bar  {
+  flex-basis: 50%;
+}
+ 
 </style>
