ff40ed7fd8dc22989e58d5cc3785b5173046d0e3
Allan Treat <allantreat73@gmail.com>
2023-02-07 12:28:46 -0500

    Locations controller almost complete

--- capstone/java/src/main/java/com/techelevator/controller/LocationsController.java
+++ capstone/java/src/main/java/com/techelevator/controller/LocationsController.java
@@ -2,10 +2,7 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.UserDao;
 import com.techelevator.dao.locations.*;
-import com.techelevator.model.locations.Area;
-import com.techelevator.model.locations.Crag;
-import com.techelevator.model.locations.State;
-import com.techelevator.model.locations.Wall;
+import com.techelevator.model.locations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -33,7 +30,7 @@ public class LocationsController {
 
 
 //@PreAuthorize("hasRole('USER')")
-@GetMapping(path="/state/")
+@GetMapping(path="/state")
 public List<State> getAllStates(){
     return stateDao.getAllStates();
 }
@@ -44,7 +41,7 @@ public State getStateByAbbrev(@PathVariable String abbrev){
     return stateDao.getStateByAbbrev(abbrev);
 }
 
-@GetMapping(path="/area/")
+@GetMapping(path="/area")
 public List<Area> getAllAreas(){
     return areaDao.getAllAreas();
 }
@@ -54,11 +51,20 @@ public Area getAreaById(@PathVariable String id){
     return areaDao.getAreaById(id);
 }
 @ResponseStatus(HttpStatus.CREATED)
-@PostMapping(path="/area/{id}")
+@PostMapping(path="/area/create")
 public Area addArea(@RequestBody Area area){
     return areaDao.addArea(area);
 }
-@GetMapping(path="/crag/")
+@PutMapping(path="/area")
+public boolean updateArea(@RequestBody Area area){
+    return areaDao.updateArea(area);
+}
+@ResponseStatus(HttpStatus.NO_CONTENT)
+@DeleteMapping(path="/area/{id}")
+public boolean deleteArea(@RequestBody String id){
+    return areaDao.deleteArea(id);
+}
+@GetMapping(path="/crag")
 public List<Crag> getAllCrags(){
     return cragDao.getAllCrags();
 }
@@ -66,11 +72,21 @@ public List<Crag> getAllCrags(){
     public Crag getCragsById(@PathVariable String id){
     return cragDao.getCragById(id);
 }
-@PostMapping(path="/crag/{id}")
+@ResponseStatus(HttpStatus.CREATED)
+@PostMapping(path="/crag/create")
 public Crag addCrag(@RequestBody Crag crag){
     return cragDao.addCrag(crag);
 }
-@GetMapping(path="/wall/")
+@PutMapping(path="/crag")
+public boolean updateCrag(@RequestBody Crag crag){
+    return cragDao.updateCrag(crag);
+}
+@ResponseStatus(HttpStatus.NO_CONTENT)
+@DeleteMapping(path="/crag/{id}")
+public boolean deleteCrag(@RequestBody String id){
+    return cragDao.deleteCrag(id);
+}
+@GetMapping(path="/wall")
     public List<Wall> getAllWalls(){
     return wallDao.getAllWalls();
 }
@@ -78,10 +94,41 @@ public Crag addCrag(@RequestBody Crag crag){
     public Wall getWallById(@PathVariable String id){
     return wallDao.getWallById(id);
 }
-@PutMapping(path="/wall/{id}")
+@ResponseStatus(HttpStatus.CREATED)
+@PostMapping(path="/wall/create")
+public Wall addWall(@RequestBody Wall wall){
+    return wallDao.addWall(wall);
+}
+@PutMapping(path="/wall")
     public boolean updateWall(@RequestBody Wall wall){
     return wallDao.updateWall(wall);
 }
-
+@ResponseStatus(HttpStatus.NO_CONTENT)
+@PutMapping(path="/wall/{id}")
+    public boolean deleteWall(@RequestBody String id){
+    return wallDao.deleteWall(id);
+}
+@GetMapping(path="/route")
+    public List<Route> getAllRoutes(){
+    return routeDao.getAllRoutes();
+}
+@GetMapping(path="/route/{id}")
+    public Route getRouteById(@PathVariable String id){
+    return routeDao.getRouteById(id);
+}
+@ResponseStatus(HttpStatus.CREATED)
+@PostMapping(path="/route/create")
+    public Route addRoute(@RequestBody Route route){
+    return routeDao.addRoute(route);
+}
+@PutMapping(path="/route")
+    public boolean updateRoute(@RequestBody Route route){
+    return routeDao.updateRoute(route);
+}
+@ResponseStatus(HttpStatus.NO_CONTENT)
+@DeleteMapping(path="/route/{id}")
+public boolean deleteRoute(@RequestBody String id){
+    return routeDao.deleteRoute(id);
+}
 
 }
