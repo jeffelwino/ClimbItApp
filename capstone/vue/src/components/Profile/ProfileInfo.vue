8177f20ec992a8929d2f811fc1ac94905de548e7
Allan Treat <allantreat73@gmail.com>
2023-02-08 16:36:01 -0500

    Working on editing profile form

--- capstone/vue/src/components/Profile/ProfileInfo.vue
+++ capstone/vue/src/components/Profile/ProfileInfo.vue
@@ -10,10 +10,7 @@
     <v-card v-if="showForm">
       <v-form ref="form">
         <v-text-field label="Name" v-model="editedProfile.name"></v-text-field>
-        <v-text-field
-          label="Location"
-          v-model="editedProfile.location"
-        ></v-text-field>
+        <v-text-field label="Location" v-model="editedProfile.location"></v-text-field>
         <v-text-field label="Bio" v-model="editedProfile.bio"></v-text-field>
         <!-- FOR UPLOADING IMAGES. CURRENTLY FOR SHOW -->
         <!-- <v-file-input
@@ -51,7 +48,7 @@ export default {
       this.showForm = !this.showForm;
     },
     updateProfileChanges() {
-      profileService.put(this.profile).then(
+      profileService.put(this.editedProfile).then(
         () => {
           window.alert("Profile Recently updated")
         }
@@ -65,19 +62,7 @@ export default {
       this.showForm = false;
     },
     cancelChanges() {
-      // this.$store.commit("UPDATE_PROFILE_PIC", {
-      //   profileId: this.$store.state.user.id,
-      //   picture: this.currentPicture,
-      // });
-      // this.editedProfile = {
-      //   id: this.$route.params.id,
-      //   name: this.profile.name,
-      //   location: this.profile.location,
-      //   bio: this.profile.bio,
-      //   todos: this.profile.todos,
-      //   picture: this.profile.picture,
-      // };
-      this.editedProfile = this.$route.params.profile.id;
+      this.editedProfile = {};
       this.showForm = false;
     },
   },
