f2a3b76de133307fcdf938a1772eb3690b1b6349
Allan Treat <allantreat73@gmail.com>
2023-02-09 09:29:44 -0500

    just in case

--- capstone/vue/src/components/Profile/ProfilePicture.vue
+++ capstone/vue/src/components/Profile/ProfilePicture.vue
@@ -7,10 +7,12 @@
           </v-btn>
       <v-row class="justify-center">
         
-        <cld-context cloudName="dacyocfmf" secure="true">
+        <cld-context v-if="profile.pictureId" cloudName="dacyocfmf" secure="true">
+          <!--My publicId is coming back as null -->
           <cld-image
+            
             :cloudName="this.$store.state.cloudName"
-            :publicId="profile.pictureId"
+            :publicId="profile.pictureId" 
             width="300"
           />
         </cld-context>
@@ -23,10 +25,12 @@
 import profileService from "../../services/ProfileService.js";
 
 export default {
+  
   // props: ["profile"],
   data() {
     return {
       profile: {},
+      showPicture: false
     };
     // computed: {
     //   profile() {
@@ -37,7 +41,9 @@ export default {
   created() {
     profileService.get(this.$route.params.id).then((response) => {
       if (response.status == 200) {
+        console.log(response.data)
         this.profile = response.data;
+        
       } else {
         this.$router.push("/notfound");
       }
--- capstone/vue/src/components/imageComps/Cloudinary.vue
+++ capstone/vue/src/components/imageComps/Cloudinary.vue
@@ -1,44 +1,66 @@
 <template>
   <v-container>
-      <v-btn v-if="this.$store.state.user.id == this.$route.params.id" v-on:click="upload">Upload your profile picture</v-btn><br>
+    <v-btn v-on:click="upload">Upload your profile picture</v-btn><br />
   </v-container>
 </template>
 
 <script>
+// import imageService from "../../services/ImageService.js"
+import profileService from "../../services/ProfileService.js";
 export default {
-  props: ["profile"],
-  name: 'cloudinary-comp',
+  // props: ["profile"],
+  name: "cloudinary-comp",
 
   data() {
     return {
-      myWidget : {}
-    }
+      myWidget: {},
+      profile: {},
+    };
   },
 
   methods: {
-      upload() {
-        this.myWidget.open();
-      }
+    upload() {
+      this.myWidget.open();
+    },
   },
   mounted() {
-       this.myWidget = window.cloudinary.createUploadWidget(
-      {
-        // Insert your cloud name and presets here, 
-        // see the documentation
-        cloudName: 'dacyocfmf',
-        uploadPreset: 'dshjquno'
-      }, 
-      (error, result) => { 
-        if (!error && result && result.event === "success") { 
-          console.log('Done! Here is the image info: ', result.info); 
-          console.log("Image URL: " + result.info.url);
-          console.log("This is the public id: " + result.info.public_id);
-          this.$store.commit("UPDATE_PROFILE_PIC", {profileId:this.$store.state.user.id, picture:result.info.public_id})
-        }
+    profileService.get(this.$route.params.id).then((response) => {
+      if (response.status == 200) {
+        this.profile = response.data;
+        this.myWidget = window.cloudinary.createUploadWidget(
+          {
+            // Insert your cloud name and presets here,
+            // see the documentation
+            cloudName: "dacyocfmf",
+            uploadPreset: "dshjquno",
+          },
+          (error, result) => {
+            if (!error && result && result.event === "success") {
+              console.log("Done! Here is the image info: ", result.info);
+              console.log("Image URL: " + result.info.url);
+              console.log("This is the public id: " + result.info.public_id);
+              this.profile.pictureId = result.info.public_id;
+              profileService.put(this.profile).then((response) => {
+                if(response.status == 200) {
+                  this.$router.go(0)
+                } else {
+                  window.alert("There was a problem...")
+                }
+              })
+              // this.$store.commit("UPDATE_PROFILE_PIC", {profileId:this.$store.state.user.id, picture:result.info.public_id})
+              // imageService.addPhoto({photoId:result.info.public_id, profileId:this.$route.params.id})
+              // .then(response => {
+              //     if(response.status == 201){
+              //         this.$router.go(0);
+              //     }
+              // })
+            }
+          }
+        );
       }
-    );
-  }
-}
+    });
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
